47553f7e90003035cbdbe812e25df709c5b33e1f
mabrouk10 <samimabrouk10@gmail.com>
2021-01-22 10:52:22 -0800

    test slack

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,17 @@
-import React from "react"
-import PropTypes from "prop-types"
-class App extends React.Component {
+import React, { Component } from 'react'
+//components
+import Header from './components/Header'
+import Footer from './components/Footer'
+//pages
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import NotFound from './pages/NotFound'
+import PostIndex from './pages/PostIndex'
+import PostProtected from './pages/PostProtected'
+import PostShow from './pages/PostShow'
+import PostNew from './pages/PostNew'
+
+class App extends Component {
   render () {
     return (
       <React.Fragment>
