e8d32d39e7caa5a7aadb195c34fb0a2ed4c4a626
Alejandro Estrada <aestrada611@gmail.com>
2021-01-25 12:00:00 -0800

    Adds seeds and controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+    def index
+      posts = Post.all
+      render json: posts
+    end
+    def create
+      post = Post.create(post_params)
+      if post.valid?
+        render json: post
+      else
+        render json: post.errors, status: :unprocessable_entity
+      end
+    end
+    # def update
+    #   post = Post.find(params[:id])
+    #   post.update(post_params)
+    #   if apartment.valid?
+    #     render json: post
+    #   else
+    #     render json: post.errors, status: :unprocessable_entity
+    #   end
+    # end
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+      if post.valid?
+        render json: post
+      else
+        render json: post.errors, status: :unprocessable_entity
+      end
+    end
+    private
+    #  Posts have: user_id, pic url, and description
+    def post_params
+      params.require(:post).permit(:user_id, :pic, :des)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create email: 'test@gmail.com', password: '123456', password_confirmation: '123456'
+posts = [
+    {
+      user_id: user.id,
+      pic: 'https://source.unsplash.com/random',
+      des: 'This is an aweomse picutre of stuff.'  
+    },
+    {
+      user_id: user.id,
+      pic: 'https://source.unsplash.com/random',
+      des: 'This is an aweomse picutre of stuff.'  
+    },
+    {
+      user_id: user.id,
+      pic: 'https://source.unsplash.com/random',
+      des: 'This is an aweomse picutre of stuff.'  
+    }
+]
+posts.each do |attr|
+    Post.create attr
+    puts "creating post: #{attr}"
+end
