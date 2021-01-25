002a168259af1e85708178ab76b91d0825863d6d
Alejandro Estrada <aestrada611@gmail.com>
2021-01-25 10:34:18 -0800

    Finishes front end and read functionality

--- app/javascript/components/pages/PostIndex.js
+++ app/javascript/components/pages/PostIndex.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react'
-// import { NavLink } from 'react-router-dom'
-// import { Button } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
+import { Button } from 'reactstrap'
 
-class PostIndex extends Component{
-  render(){
-    return(
-      <React.Fragment>
-      <h3>All the Posts</h3>
-        <div>
-          { this.props.posts && this.props.posts.map((post, index) => {
-            return (
-              <div key={ index }>
-                <img src={post.pic}></img>
-                <h5>Description: { post.des }</h5>
-                <h5>ID: { post.id }</h5>
-                <h5>User ID: { post.user_id }</h5>
-              </div>
-            )
-          })}
+class PostIndex extends Component {
+	render() {
+		const { post } = this.props
+		return (
+			<React.Fragment>
+				<h3>All the Posts</h3>
+				<div>
+					{this.props.posts &&
+						this.props.posts.map((post, index) => {
+							return (
+								<div key={index}>
+									<img src={post.pic}></img>
+									<h5>Description: {post.des}</h5>
+									<h5>ID: {post.id}</h5>
+									<h5>User ID: {post.user_id}</h5>
+									<Button>
+										<NavLink to={`/postshow/${post.id}`}>
+											Click to see post!
+										</NavLink>
+									</Button>
+								</div>
+							)
+						})}
 
-          {/* make a button to take you to show page of individual post */}
-        </div>
-      </React.Fragment>
-    )
-  }
+					{/* make a button to take you to show page of individual post */}
+				</div>
+			</React.Fragment>
+		)
+	}
 }
 export default PostIndex
