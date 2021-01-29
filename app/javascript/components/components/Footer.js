18521b4007cfb6d4f63a0b0e248af65bbd155647
priscila2294 <priscilla_2294@hotmail.com>
2021-01-29 11:52:01 -0800

    adds styling to buttons

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
+
 
 class Footer extends Component {
     render() {
         return (
-            <div>
-                <h1>Footer</h1>
-            </div>
-        );
+            <Fragment>
+                <footer className='mt-auto' id= 'footer'>
+                    <div>
+                        <a href='#top'>
+                            Return Top
+                        </a>
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        <p> Sami, Alejandro & Priscila | &#169; 2021</p>
+                    </div>
+                </footer>
+            </Fragment>
+        )
     }
 }
-
-export default Footer;
+export default Footer
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
 						{logged_in && (
 							<>
 								<NavLink className='header-link' href='/postprofile'>
-									My Post
+									My Posts
 								</NavLink>
 								<NavLink className='header-link' href='/postnew'>
 									New Post
