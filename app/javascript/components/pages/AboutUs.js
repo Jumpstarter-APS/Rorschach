dfed7cbd6c4e7d73a358185dc03fde0d3b6f3c16
Alejandro Estrada <aestrada611@gmail.com>
2021-02-04 22:25:39 -0800

    please

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -18,112 +18,117 @@ class AboutUs extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<img src={blacksand} className='bg'></img>
-				</div>
-
-				<h1 className='title-formating'>About Us</h1>
-
-				<>
-					<div
-						className='cats-display-wrapper'
-						style={{ paddingBottom: '100px' }}
-					>
-						<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
-							<img src={sami} style={{ width: '100%' }} />
-							<div id='post-description-wrapper'>
-								<h4>
-									<br></br>
-								</h4>
-								<br></br>
-								<div>
-									<ListGroupItemText>
-										{' '}
-										<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
-											Sami
-										</h1>
-									</ListGroupItemText>
-									<h3>
-										{' '}
-										<a id='icons' href='https://www.linkedin.com/in/mabrouk10/'>
-											{' '}
-											<FaLinkedin />{' '}
-										</a>
-										<a id='icons' href='https://github.com/mabrouk10'>
-											{' '}
-											<FaGithub />{' '}
-										</a>
-									</h3>
-								</div>
-							</div>
-						</ListGroup>
-
-						<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
-							<img src={Alejandro} style={{ width: '100%' }} />
-							<div id='post-description-wrapper'>
-								<h4>
-									<br></br>
-								</h4>
-								<br></br>
-								<div>
-									<ListGroupItemText>
-										{' '}
-										<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
-											Alejandro
-										</h1>
-									</ListGroupItemText>
-									<h3>
-										{' '}
-										<a
-											id='icons'
-											href='https://www.linkedin.com/in/alejandroestrada617/'
-										>
-											{' '}
-											<FaLinkedin />{' '}
-										</a>
-										<a id='icons' href='https://github.com/aestrada611'>
-											{' '}
-											<FaGithub />{' '}
-										</a>
-									</h3>
-								</div>
-							</div>
-						</ListGroup>
-
-						<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
-							<img src={priscila} style={{ width: '100%' }} />
-							<div id='post-description-wrapper'>
-								<h4>
-									<br></br>
-								</h4>
-								<br></br>
-								<div>
-									<ListGroupItemText>
-										{' '}
-										<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
-											Priscila
-										</h1>
-									</ListGroupItemText>
-
-									<h3>
-										<a
-											id='icons'
-											href='https://www.linkedin.com/in/priscilap-developer/'
-										>
-											{' '}
-											<FaLinkedin />{' '}
-										</a>
-
-										<a id='icons' href='https://github.com/priscila2294'>
-											{' '}
-											<FaGithub />{' '}
-										</a>
-									</h3>
-								</div>
-							</div>
-						</ListGroup>
+				<div style={{ height: '100vh' }}>
+					<div>
+						<img src={blacksand} className='bg'></img>
 					</div>
-				</>
+
+					<h1 className='title-formating'>About Us</h1>
+
+					<>
+						<div
+							className='cats-display-wrapper'
+							style={{ paddingBottom: '100px' }}
+						>
+							<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
+								<img src={sami} style={{ width: '100%' }} />
+								<div id='post-description-wrapper'>
+									<h4>
+										<br></br>
+									</h4>
+									<br></br>
+									<div>
+										<ListGroupItemText>
+											{' '}
+											<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
+												Sami
+											</h1>
+										</ListGroupItemText>
+										<h3>
+											{' '}
+											<a
+												id='icons'
+												href='https://www.linkedin.com/in/mabrouk10/'
+											>
+												{' '}
+												<FaLinkedin />{' '}
+											</a>
+											<a id='icons' href='https://github.com/mabrouk10'>
+												{' '}
+												<FaGithub />{' '}
+											</a>
+										</h3>
+									</div>
+								</div>
+							</ListGroup>
+
+							<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
+								<img src={Alejandro} style={{ width: '100%' }} />
+								<div id='post-description-wrapper'>
+									<h4>
+										<br></br>
+									</h4>
+									<br></br>
+									<div>
+										<ListGroupItemText>
+											{' '}
+											<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
+												Alejandro
+											</h1>
+										</ListGroupItemText>
+										<h3>
+											{' '}
+											<a
+												id='icons'
+												href='https://www.linkedin.com/in/alejandroestrada617/'
+											>
+												{' '}
+												<FaLinkedin />{' '}
+											</a>
+											<a id='icons' href='https://github.com/aestrada611'>
+												{' '}
+												<FaGithub />{' '}
+											</a>
+										</h3>
+									</div>
+								</div>
+							</ListGroup>
+
+							<ListGroup className='cat-wrapper' style={{ width: '300px' }}>
+								<img src={priscila} style={{ width: '100%' }} />
+								<div id='post-description-wrapper'>
+									<h4>
+										<br></br>
+									</h4>
+									<br></br>
+									<div>
+										<ListGroupItemText>
+											{' '}
+											<h1 style={{ textAlign: 'center', fontSize: '3rem' }}>
+												Priscila
+											</h1>
+										</ListGroupItemText>
+
+										<h3>
+											<a
+												id='icons'
+												href='https://www.linkedin.com/in/priscilap-developer/'
+											>
+												{' '}
+												<FaLinkedin />{' '}
+											</a>
+
+											<a id='icons' href='https://github.com/priscila2294'>
+												{' '}
+												<FaGithub />{' '}
+											</a>
+										</h3>
+									</div>
+								</div>
+							</ListGroup>
+						</div>
+					</>
+				</div>
 				<Footer />
 			</div>
 		)
